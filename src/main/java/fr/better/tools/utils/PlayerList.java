69f1638b506b4218f71c06d94d71536d66034959
Gabriel Quéré <65867762+betterstudio@users.noreply.github.com>
2020-12-13 19:37:39 +0100

    1.6.3

---
+++ src/main/java/fr/better/tools/utils/PlayerList.java
@@ -0,0 +1,54 @@
+package fr.better.tools.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+public class PlayerList {
+
+    private final List<UUID> data;
+
+    public PlayerList() {
+        this.data = new ArrayList<>();
+    }
+
+    public void add(Player key){
+        data.add(key.getUniqueId());
+    }
+
+    public void remove(Player key){
+        data.remove(key);
+    }
+
+    public int posOf(Player key){
+        return data.indexOf(key.getUniqueId());
+    }
+
+    public boolean exist(Player key){
+        return data.contains(key.getUniqueId());
+    }
+
+    public List<Player> toPlayers(){
+        List<Player> players = new ArrayList<>();
+        for(UUID u : data){
+
+            Player p = Bukkit.getPlayer(u);
+
+            if(p == null){
+                data.remove(u);
+            }
+
+            players.add(p);
+        }
+        return players;
+    }
+
+
+    public List<UUID> toUUIDs() {
+        return data;
+    }
+}
