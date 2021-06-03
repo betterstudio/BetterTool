7415d87cec641568d4f10ef92516acdbc0baf3a6
betterstudio <65867762+betterstudio@users.noreply.github.com>
2021-06-03 22:15:25 +0200

    Update Gui.java

--- src/main/java/fr/better/tools/inventory/gui/Gui.java
+++ src/main/java/fr/better/tools/inventory/gui/Gui.java
@@ -11,6 +11,7 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public class Gui {
 
@@ -30,32 +31,47 @@ public class Gui {
         return new Gui(inv);
     }
 
-    public void open(Player player) {
+    public Gui open(Player player) {
         player.openInventory(inventory);
+        return this;
     }
 
     public String name() {
         return inventory.getTitle();
     }
 
-    public void click(BiConsumer<InventoryClickEvent, Player> action){
+    public Gui click(BiConsumer<InventoryClickEvent, Player> action){
         this.click = action;
+        return this;
     }
 
-    public void close(BiConsumer<InventoryCloseEvent, Player> action){
+    public Gui close(BiConsumer<InventoryCloseEvent, Player> action){
         this.close = action;
+        return this;
     }
 
-    public void setClickCancelled(){
+    public Gui click(Consumer<InventoryClickEvent> action){
+        this.click = (event, player) -> action.accept(event);
+        return this;
+    }
+
+    public Gui close(Consumer<InventoryCloseEvent> action){
+        this.close = (event, player) -> action.accept(event);
+        return this;
+    }
+
+    public Gui setClickCancelled(){
         this.click = (e,p) -> {
             e.setCancelled(true);
         };
+        return this;
     }
 
-    public void setCantClose(){
+    public Gui setCantClose(){
         this.close = (e,p) -> {
             e.getPlayer().openInventory(inventory);
         };
+        return this;
     }
 
     public Inventory getInventory(){
@@ -70,27 +86,31 @@ public class Gui {
         return close;
     }
 
-    public void setTopLine(int data){
+    public Gui setTopLine(int data){
         setTopLine(new ItemCreate(Material.STAINED_GLASS_PANE, 1, (short) data).setName(" ").build());
+        return this;
     }
 
-    public void setTopLine(ItemStack item){
+    public Gui setTopLine(ItemStack item) {
         for(int i = 0; i < 9; i++){
             inventory.setItem(i, item);
         }
+        return this;
     }
 
-    public void setBackLine(int data){
+    public Gui setBackLine(int data){
         setBackLine(new ItemCreate(Material.STAINED_GLASS_PANE, 1, (short) data).setName(" ").build());
+        return this;
     }
 
-    public void setBackLine(ItemStack item){
+    public Gui setBackLine(ItemStack item){
         for(int i = inventory.getSize()-9; i < inventory.getSize(); i++){
             inventory.setItem(i, item);
         }
+        return this;
     }
 
-    public void setThePerfect(int data){
+    public Gui setThePerfect(int data){
         try{
             ItemStack item = new ItemCreate(Material.STAINED_GLASS_PANE, 1, (short) data).setName(" ").build();
 
@@ -100,21 +120,24 @@ public class Gui {
         }catch(Exception ex){
             System.out.println("You can use that uniquely on a 6 line sized chest !");
         }
+        return this;
     }
 
-    public void setFull(ItemStack what){
+    public Gui setFull(ItemStack what){
         for(int i = 0; i < inventory.getSize(); i++){
             inventory.setItem(i, what);
         }
+        return this;
     }
 
-    public void setFull(int color){
+    public Gui setFull(int color){
         for(int i = 0; i < inventory.getSize(); i++){
             inventory.setItem(i,  new ItemCreate(Material.STAINED_GLASS_PANE, 1, (short) color).setName(" ").build());
         }
+        return this;
     }
 
-    public void setSideWays(ItemStack item){
+    public Gui setSideWays(ItemStack item){
         switch(inventory.getSize()/9){
             case 6:
                 inventory.setItem(45, item);
@@ -136,73 +159,27 @@ public class Gui {
                 inventory.setItem(8, item);
                 break;
         }
+        return this;
     }
 
-    public void setSideWays(int data){
+    public Gui setSideWays(int data){
         setSideWays(new ItemCreate(Material.STAINED_GLASS_PANE, 1, (short) data).setName(" ").build());
+        return this;
     }
 
-    public void set(ItemStack what, int... where){
+    public Gui set(ItemStack what, int... where){
         for(int i : where){
             inventory.setItem(i, what);
         }
+        return this;
     }
 
-    public void add(ItemStack... item){
+    public Gui add(ItemStack... item){
         for(ItemStack i : item)inventory.addItem(i);
+        return this;
     }
 
     public Inventory give() {
         return inventory;
-    }
-
-    public static class Builder{
-        private final Gui gui;
-
-        public Builder(Gui gui) {
-            this.gui = gui;
-        }
-
-        public Builder topLine(int data){
-            gui.setTopLine(data);return this;
-        }
-
-        public Builder backline(int data){
-            gui.setBackLine(data);return this;
-        }
-
-        public Builder sideway(int data){
-            gui.setSideWays(data);return this;
-        }
-
-        public Builder set(ItemStack stack, int slot){
-            gui.set(stack, slot);return this;
-        }
-
-        public Builder add(ItemStack stack){
-            gui.add(stack);return this;
-        }
-
-        public Builder full(int data){
-            gui.setFull(data);return this;
-        }
-
-        public Builder click(BiConsumer<InventoryClickEvent, Player> event){
-            gui.click(event);return this;
-        }
-
-        public Builder close(BiConsumer<InventoryClickEvent, Player> event){
-            gui.click(event);return this;
-        }
-
-        public Builder cantClose(){
-            gui.setCantClose();return this;
-        }
-
-        public Builder cantClick(){
-            gui.setClickCancelled();return this;
-        }
-
-        public Gui get(){return gui;}
     }
 }
